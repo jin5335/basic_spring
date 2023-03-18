a9a2568c5e478e00fc9f93a866ce550db306f1c7
jin5335 <67370093+jin5335@users.noreply.github.com>
2023-03-18 14:52:37 +0900

    [ch2] 예제만들기

--- core/src/main/java/hello/core/discount/DiscountPolicy.java
+++ core/src/main/java/hello/core/discount/DiscountPolicy.java
@@ -3,12 +3,8 @@ package hello.core.discount;
 import hello.core.member.Member;
 
 public interface DiscountPolicy {
-
-    /**
-     *
-     * @param member
-     * @param price
-     * @return 할인 대상 금액
-     */
-    int discount(Member member, int price);
+	/**
+	 * @return 할인 대상 금액
+	 */
+	int discount(Member member, int price);
 }
